76172b686c74c3dd392faf7ead35b25b165b8c09
passive070-cmyk <passive070@gmail.com>
2026-02-12 10:04:14 +0530

    Create style.css

---
+++ style.css
@@ -0,0 +1,29 @@
+body {
+    text-align: center;
+    font-family: Arial, sans-serif;
+    background-color: #ffe6f0;
+}
+
+.container {
+    margin-top: 100px;
+}
+
+button {
+    padding: 15px 30px;
+    font-size: 18px;
+    margin: 10px;
+    cursor: pointer;
+    border-radius: 10px;
+    border: none;
+}
+
+#yesBtn {
+    background-color: #ff4d88;
+    color: white;
+}
+
+#noBtn {
+    background-color: #666;
+    color: white;
+    position: relative;
+}
